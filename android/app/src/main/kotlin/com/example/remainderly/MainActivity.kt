09a4b6ba55cc1bc1de831ab43e44cc242e1bef9b
aatitkarki <aatitkarki12345@gmail.com>
2021-04-14 08:50:16 +0545

    added clean code architecture and migrated to flutter 2.0'

---
+++ android/app/src/main/kotlin/com/example/remainderly/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.remainderly
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- android/app/src/main/kotlin/com/sureshk/TodoApp/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.sureshk.TodoApp
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
